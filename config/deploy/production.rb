519ea900b90689424cca11657e77ad590175e7de
Arun Tekwani <aruntekwani4567@gmail.com>
2023-06-17 20:43:03 +0530

    testing

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
-server ENV['54.174.174.34'], user: 'deploy', roles: %w[app db web], primary: 'true'
-set :deploy_to, 'home/deploy/vidos'
+server '54.174.174.34', user: 'deploy', roles: %w[app db web], primary: 'true'
+# set :deploy_to, 'home/deploy/vidos'
 set :branch, 'master'
 set :stage, :production
 set :rails_env, :production
